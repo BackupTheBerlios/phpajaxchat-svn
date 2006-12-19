c12ab8b0427c92661efd2a75fe44d42bce685ea5
coolwebteacher <coolwebteacher@f116aeae-f824-0410-8b3a-9c868b4fbe04>
2006-12-19 21:08:36 +0000

    M    connect2.php

--- connect2.php
+++ connect2.php
@@ -1,9 +1,9 @@
 <?
 $server = 'localhost';
-$username = 'root';
-$password = 'i81uo22';
-$database_name = 'khansen';
-$chatname="khansen";
+$username = '';
+$password = '';
+$database_name = '';
+$chatname='';
 $connection=mysql_connect($server, $username, $password);
 mysql_select_db($database_name,$connection);
 ?>
